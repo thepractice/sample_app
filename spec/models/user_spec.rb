0123cc97a4a23457ed055eb1e8994e712b913c04
edward <edwardhornig@gmail.com>
2014-03-04 21:44:39 -0500

    added line has_secure_password to app/models/user.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,18 @@ require 'spec_helper'
 
 describe User do
   
-  before { @user = User.new(name: 'Example User', email: 'user@example.com') }
+  before do
+    @user = User.new(name: 'Example User', email: 'user@example.com',
+                     password: 'foobar', password_confirmation: 'foobar')
+  end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to (:email) }
+  it { should respond_to (:password_digest) }
+  it { should respond_to (:password) }
+  it { should respond_to (:password_confirmation) }
 
   it { should be_valid }
 
@@ -56,4 +62,19 @@ describe User do
 
     it { should_not be_valid }
   end
+
+  describe 'when password is not present' do
+    before do
+      @user.password = ' '
+      @user.password_confirmation = ' '
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe 'when password does not match confirmation' do
+    before { @user.password_confirmation = 'mismatch' }
+    it { should_not be_valid }
+  end
+
 end
