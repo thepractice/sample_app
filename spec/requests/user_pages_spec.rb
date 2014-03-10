f83000cbda5912462499bcb16b7d98a73960fe0e
edward <edwardhornig@gmail.com>
2014-03-10 13:41:40 -0400

    Complete signin

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,9 +25,35 @@ describe 'User pages' do
 
     let(:submit) { "Create my account" }
 
-    describe "with invalid information" do
+    describe "with invalid (blank) information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_selector('div.alert-error', text: 'error') }
+        it { should have_content('blank') }
+      end
+    end
+
+    describe "with non-matching password confirmation" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar2"
+      end
+
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+        it { should have_content('match') }
       end
     end
 
@@ -41,6 +67,14 @@ describe 'User pages' do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
