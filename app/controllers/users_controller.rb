edd3a25a94e08d796f0d4e44e7de6f33854099ec
edward <edwardhornig@gmail.com>
2014-03-15 19:13:08 -0400

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
   
   def show
   	@user = User.find(params[:id])
   end
 
   def index
-    @users = User.all
+    @users = User.order(:id).paginate(page: params[:page]) # Added 'order(:id)' to get first user to show first.
   end
 
   def new
@@ -37,6 +38,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+
   private
 
     def user_params
@@ -56,5 +63,9 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
